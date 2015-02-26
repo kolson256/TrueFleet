1395a09431bb09ef506ad96ca3d9f0938fd1ee52
Richard Morgan <rmorgan15@gmail.com>
2015-02-26 01:03:03 -0600

    working on Hibernate tests

--- TruFleetWebServices/src/test/java/com/trufleet/service/login/hibernate/TestHibernateAccount.java
+++ TruFleetWebServices/src/test/java/com/trufleet/service/login/hibernate/TestHibernateAccount.java
@@ -1,15 +1,21 @@
 package com.trufleet.service.login.hibernate;
 
-import com.fasterxml.jackson.core.JsonParseException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trufleet.services.dao.AccountEntityDAO;
 import com.trufleet.services.domain.representations.AccountEntity;
 import io.dropwizard.jackson.Jackson;
-import org.apache.shiro.authc.Account;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.fail;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,6 +25,9 @@ import java.io.IOException;
  */
 public class TestHibernateAccount extends BaseHibernateTest {
 
+    private SessionFactory sessionFactory;
+    private Session session;
+    private Transaction tx;
     AccountEntityDAO dao;
 
     private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
@@ -26,32 +35,83 @@ public class TestHibernateAccount extends BaseHibernateTest {
 
     public TestHibernateAccount(){
         super();
-        dao = new AccountEntityDAO(factory);
+    }
+
+    @Before
+    public void setup(){
+        sessionFactory = getSessionFactory();
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+        dao = new AccountEntityDAO(sessionFactory);
+
     }
 
 
+//
 //    @Test
-//    public void PersistNewAccount() throws IOException {
+//    public void TestPersistNewAccount() throws IOException {
 //
 //        File file = new File("src/test/resources/fixtures/account-no-id.json");
 //
-//        AccountEntity accountEntity = MAPPER.readValue(file, AccountEntity.class);
+//        try {
 //
-//        logger.debug("JSON Read, Account Entity Object Account Name is: " + accountEntity.getName());
+//            AccountEntity accountEntity = MAPPER.readValue(file, AccountEntity.class);
+//            logger.debug("JSON Read, Account Entity Object Account Name is: " + accountEntity.getName());
 //
-//        int returnValue = dao.create(accountEntity);
+//            session.saveOrUpdate(checkNotNull(accountEntity));
 //
-//        assertThat(returnValue).isNotNull();
+//            int returnValue = dao.create(accountEntity);
 //
-//        logger.debug("Account Entity Object Persisted ID returned is: " + returnValue);
-//        logger.debug("Attempting Retreival of new Account via ID.");
+//            assertThat(returnValue).isNotNull();
 //
-//        AccountEntity retrievedAccount = dao.findById(returnValue);
+//            logger.debug("Account Entity Object Persisted ID returned is: " + returnValue);
 //
-//        assertThat(returnValue).isEqualTo(retrievedAccount.getId());
 //
-//        logger.debug("Retrieved AccountEntity ID is: " + retrievedAccount.getId() + " and name is: " + retrievedAccount.getName());
+//        }catch (Exception ex) {
 //
+//            ex.printStackTrace();
+//            if( tx != null){
+//                tx.rollback();
+//            }
+//
+//            fail("Exception Thrown");
+//        }
+//        finally {
+//            if (session != null) {
+//                session.close();
+//            }
+//        }
+//
+//    }
+
+
+//    @Test
+//    public void TestGetAccountByID(){
+//
+//        Session session;
+//        Transaction tx;
+//
+//        session = factory.openSession();
+//        tx = session.beginTransaction();
+//
+//        try {
+//
+//            logger.debug("Attempting Retrieval of Account via ID.");
+//            AccountEntity retrievedAccount = dao.findById(returnValue);
+//
+//            assertThat(returnValue).isEqualTo(retrievedAccount.getId());
+//
+//            logger.debug("Retrieved AccountEntity ID is: " + retrievedAccount.getId() + " and name is: " + retrievedAccount.getName());
+//
+//            tx.commit();
+//
+//
+//
+//        }catch (Exception ex) {
+//            ex.printStackTrace();
+//            tx.rollback();
+//        }
+//        finally {session.close();}
 //
 //    }
 
