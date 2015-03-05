a66e24928a26e1ec444042215085b7e4222a067d
Chris Lacy <chris.c.lacy@gmail.com>
2015-03-04 21:02:26 -0600

    API update

--- TrueFleetAndroid/app/src/main/java/app/truefleet/com/truefleet/Tasks/ApiService.java
+++ TrueFleetAndroid/app/src/main/java/app/truefleet/com/truefleet/Tasks/ApiService.java
@@ -5,12 +5,16 @@ import com.squareup.okhttp.Route;
 import org.json.JSONObject;
 
 import app.truefleet.com.truefleet.Models.Account;
+import app.truefleet.com.truefleet.Models.Contact;
 import app.truefleet.com.truefleet.Models.Container;
+import app.truefleet.com.truefleet.Models.Freight;
+import app.truefleet.com.truefleet.Models.Linehaul;
 import app.truefleet.com.truefleet.Models.Order;
 import app.truefleet.com.truefleet.Models.User;
 import app.truefleet.com.truefleet.Tasks.Requests.GcmRegisterRequest;
 import app.truefleet.com.truefleet.Tasks.Requests.LoginRequest;
 import retrofit.http.Body;
+import retrofit.http.GET;
 import retrofit.http.Header;
 import retrofit.http.Headers;
 import retrofit.http.POST;
@@ -35,15 +39,24 @@ public interface ApiService {
     public void registerGcmWithServer(@Header("authToken") String authToken, @Header("tenantId") String tenantId,
                                       @Body GcmRegisterRequest gcmRegisterRequest, RestCallback<JSONObject> cb);
 
-    @POST("/0.1/order/{id}")
+    @GET("/0.1/order/{id}")
     public void getOrder(@Path("id") int orderId, RestCallback<Order> cb);
 
-    @POST("/0.1/order/{id}")
+    @GET("/0.1/account/{id}")
     public void getAccount(@Path("id") int accountId, RestCallback<Account> cb);
 
-    @POST("/0.1/order/{id}")
+    @GET("/0.1/container/{id}")
     public void getContainer(@Path("id") int containerId, RestCallback<Container> cb);
 
-    @POST("/0.1/route/{id}")
+    @GET("/0.1/route/{id}")
     public void getRoute(@Path("id") int routeId, RestCallback<Route> cb);
+
+    @GET("/0.1/linehaul/{id}")
+    public void getLinehaul(@Path("id") int routeId, RestCallback<Linehaul> cb);
+
+    @GET("/0.1/freight/{id}")
+    public void getFreight(@Path("id") int routeId, RestCallback<Freight> cb);
+
+    @GET("/0.1/contact/{id}")
+    public void getContact(@Path("id") int routeId, RestCallback<Contact> cb);
 }
